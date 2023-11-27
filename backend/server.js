2c447733e102c20418e4d5f4abbc28d8b6ec1516
Sanerita <sanelisiwe.sileku@gmail.com>
2023-11-27 17:31:11 +0200

    installed dotenv and set up get request by id

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { chats } = require("./data/data");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.get('/', (req, res) => {
 
@@ -12,5 +14,15 @@ app.get("/api/chat", (req, res) => {
 
     res.send(chats);
 });
+
+app.get("/api/chat/:id", (req, res) => {
+
+    // console.log(req.params.id);
+
+    const singlechat = chats.find(c=>c._id===req.params.id);
+    res.send(singlechat);
+});
+
+const PORT = process.env.PORT || 5000
 
 app.listen(5000, console.log("Server started on port 5000"))
