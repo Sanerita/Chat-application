77e87d9c0ac32bcb25f6e22905a0b099c975c012
Sanerita <sanelisiwe.sileku@gmail.com>
2023-11-27 16:21:56 +0200

    added database module

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,14 @@ const express = require("express");
 
 const app = express()
 
+app.get('/', (req,res) => {
+
+    res.send("API is running");
+});
+
+app.get('/api/chat', (req,res) => {
+
+ res.send(chats);
+});
+
 app.listen(5000, console.log("Server started on port 5000"))
