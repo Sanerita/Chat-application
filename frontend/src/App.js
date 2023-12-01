c253031ad1be36d24fbce78d64b4706027e1a089
Sanerita <sanelisiwe.sileku@gmail.com>
2023-12-01 11:47:00 +0200

    installed react scripts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import { Route} from 'react-router-dom';
-import landingPage from './pages/landingPage';
-// import homePage from './pages/homePage'
+import { Routes, Route} from 'react-router-dom';
+import LandingPage from './pages/landingPage';
+import HomePage from './pages/homePage'
 
 
 function App() {
   return (
     <div className="App">
-
-    
-      
-      <Route path="/" Component={landingPage} />
-
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/homePage" element={<HomePage />} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
     </div>
   );
 }
