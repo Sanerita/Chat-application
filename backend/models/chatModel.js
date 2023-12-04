4cc7f0b0b1c19571bb544a2725ae1c54d467f95f
Sanerita <sanelisiwe.sileku@gmail.com>
2023-12-04 18:38:09 +0200

    created chat model

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,4 +1,33 @@
-// chatName
-// isGroupChat
-// latestMessag
-// groupAdmin
+const mongoose = require('mongoose')
+
+const chatModel = mongoose.Schema({
+  chatName: { type: String, trim: true },
+  isGroupChat: { type: Boolean, default: false },
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  latestMessage:
+  { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Message"
+
+  },
+  groupAdmin:
+  {
+    type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+  },
+
+},
+{
+timestamps:true,
+
+});
+
+const chat = mongoose.model("chat", chatModel);
+
+module.exports = chat;
+
