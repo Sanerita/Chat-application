d0dcb1a87726760420b3084f9aef8912e99ef555
Sanerita <sanelisiwe.sileku@gmail.com>
2023-12-08 14:26:30 +0200

    added a navbar on the landing page

--- frontend/src/pages/landingPages.jsx
+++ frontend/src/pages/landingPages.jsx
@@ -1,17 +1,25 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Input, Button, Image } from "@chakra-ui/react";
 import React from "react";
 
-const landingPages = () => {
+const LandingPage = () => {
+  const handleSearchClick = () => {
+    // Add logic for when the search bar is clicked
+    console.log("Search bar clicked!");
+  };
+
+  const handleSignInClick = () => {
+    // Add logic for when the sign-in button is clicked
+    console.log("Sign-in button clicked!");
+  };
+
   return (
     <Box
-
       position="relative"
-      top="0"
-      left="0"
       width="100%"
       height="100%"
       overflow="hidden"
       zIndex="-1"
+
     >
       <video
         autoPlay
@@ -30,20 +38,50 @@ const landingPages = () => {
         />
         Your browser does not support the video tag.
       </video>
+      <Flex
+        position="fixed"
+        top="0"
+        left="0"
+        right="0"
+        alignItems="center"
+        justifyContent="space-between"
+        p={4}
+        bg="rgba(136, 216, 192, 0.8)"
+        zIndex="1"
+      >
+        <Box top="0" left="10" alignItems="center" justifyContent="space-between" bgSize="50p">
+          <Image
+            src="./src/assets/TOAST-removebg-preview.png"
+            alt="Logo"
+            boxSize="60px"
+            maxW="60px"
+            maxH="5060px"
 
+          />
+
+        </Box>
+        <Box onClick={handleSearchClick}>
+
+          <Input placeholder="Search..." mx="auto" />
+
+        </Box>
+        <Button variant="outline" mr={4} bg="#246E58" onClick={handleSignInClick}>
+          Sign In as Guest
+        </Button>
+      </Flex >
       <Box
         position="absolute"
-        top="55%"
+        top="50%"
         left="50%"
         transform="translate(-50%, -50%)"
       /* Additional styling for your content */
       >
-        {/* Your login page content goes here */}
+        {/* Your landing page content goes here */}
         <h1>Landing Page</h1>
         {/* ... */}
       </Box>
-    </Box>
+    </Box >
   );
 };
 
-export default landingPages;
+export default LandingPage;
